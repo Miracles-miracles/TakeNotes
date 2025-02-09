be3ee8f0b6e6506a530cd19afc59b678f597f508
Miracles-miracles <2663284851@qq.com>
2025-02-09 14:48:21 +0800

    first

---
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -0,0 +1,1 @@
+export const siteData = JSON.parse("{\"base\":\"/TakeNotes/\",\"lang\":\"en-US\",\"title\":\"TakeNotes\",\"description\":\"\",\"head\":[],\"locales\":{}}")
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  bundler: viteBundler(),
+  title: 'TakeNotes',
+  base: '/TakeNotes/',
+
+  theme:defaultTheme({
+    
+    logo: '/.vuepress/public/images/NV.jpg',
+
+    navbar: [
+        {text: '首页', link: '/'},
+        {text: '学习Linux', link: '/linuxLearning/'},
+        {text: '学习Java', link: '/javaLearning/'},
+    ]
+  })
+})
